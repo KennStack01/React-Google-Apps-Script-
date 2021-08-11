f0d495965f239ebcda971798e3d1c324ecffacfc
Kenn Stack <kibadikennedy@gmail.com>
2021-08-11 19:02:07 +0800

    added headless ui

--- src/client/logic-sheet-ui/components/Logic.jsx
+++ src/client/logic-sheet-ui/components/Logic.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
+import App from './App';
 
 const Logic = () => (
   <div>
     <p>
       <b className="text-blue-600">Welcome to Logic Sheet ⚽</b>
     </p>
-    <p>
-      This is a very simple page demonstrating how to build a React app inside a
-      sidebar.
-    </p>
-    <p>
-      Visit the Github repo for more information on how to use this project.
-    </p>
-    <p>- Elisha Nuchi</p>
-    <a
-      href="https://www.github.com/enuchi/React-Google-Apps-Script"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      React + Google Apps Script
-    </a>
+    <App />
   </div>
 );
 
